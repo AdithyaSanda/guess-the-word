82af35ba74e7e9ea8112aa3d272c449557737c17
AdithyaSanda <adithyasanda2006@gmail.com>
2026-08-10 20:36:19 +0530

    Added Repositories

---
+++ backend/guess-the-word/src/main/java/com/guessword/guess_the_word/repositories/WordRepository.java
@@ -0,0 +1,10 @@
+package com.guessword.guess_the_word.repositories;
+
+import com.guessword.guess_the_word.entities.Word;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WordRepository extends JpaRepository<Word, Long> {
+
+}
