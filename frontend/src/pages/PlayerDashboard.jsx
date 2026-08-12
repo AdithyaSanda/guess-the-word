23cec1fd31760e0a0961af4d067bd576b12b2edf
AdithyaSanda <adithyasanda2006@gmail.com>
2026-08-12 23:35:03 +0530

    added Player dashboard UI

--- frontend/src/pages/PlayerDashboard.jsx
+++ frontend/src/pages/PlayerDashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../services/api';
+import wordQuest from "../assets/wordQuest.png";
+import wordQuestLogo from "../assets/wordQuestLogo.png"
 
 const PlayerDashboard = () => {
 
@@ -10,6 +12,7 @@ const PlayerDashboard = () => {
 
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+    const [showLimitModal, setShowLimitModal] = useState(false);
 
     const startGame = async () => {
         setError("");
@@ -24,6 +27,10 @@ const PlayerDashboard = () => {
         }
         catch(error) {
             setError(error.response?.data?.message || "Unable to start game.");
+
+            if (error.response?.status === 400 || error.response?.status === 403) {
+                setShowLimitModal(true);
+            }
         }
         finally {
             setLoading(false);
@@ -39,23 +46,92 @@ const PlayerDashboard = () => {
     }
 
     return (
-        <div>
-            <header>
-                <h1>Guess The Word</h1>
-                <button onClick={logout}>Logout</button>
-            </header>
+        <div className='w-full min-h-screen flex flex-col items-center justify-center gap-4'>
+          
+                <img src={wordQuestLogo} alt="wordQuestLogo" className='w-30'/>
+                <img src={wordQuest} alt='wordQuest' className='w-100 h-auto object-contain'/>
+                <h2 className='text-4xl font-semibold'>Welcome, {username}</h2>
 
-            <main>
-                <h2>Welcome, {username}</h2>
-
-                <p>Guess the 5-letter word.</p>
-
-                <p>You can play upto 3 games per day.</p>
+                <div className='flex flex-col items-center gap-2 mt-2'>
+                    <p className='text-2xl'>Guess the 5-letter word.</p>
+                    <p className='text-2xl'>You can play upto 3 games per day.</p>
+                </div>
+                
 
                 {error && <p>{error}</p>}
 
-                <button onClick={startGame} disabled={loading}>{loading ? "Starting Game..." : "Start Game"}</button>
-            </main>
+                <div className='flex gap-2 mt-4'>
+                    <button className='border border-white p-3 w-40 rounded-full hover:bg-[#3a3a3c] font-semibold cursor-pointer' onClick={logout}>Logout</button>
+                    <button className='border border-white p-3 w-40 rounded-full hover:bg-[#3a3a3c] font-semibold cursor-pointer' onClick={startGame} disabled={loading}>{loading ? "Starting Game..." : "Start Game"}</button>
+                </div>
+
+
+                {showLimitModal && (
+                <div
+                    className="
+                        fixed
+                        inset-0
+                        z-50
+                        flex
+                        items-center
+                        justify-center
+                        bg-black/60
+                        px-4
+                    "
+                >
+
+                    <div
+                        className="
+                            w-full
+                            max-w-md
+                            rounded-xl
+                            border-2
+                            border-[#3a3a3c]
+                            bg-[#181818]
+                            p-8
+                            text-center
+                            shadow-2xl
+                        "
+                    >
+
+                        <div className="mb-4 text-4xl">
+                            🎮
+                        </div>
+
+                        <h2 className="text-2xl font-bold text-white">
+                            Daily Limit Reached
+                        </h2>
+
+                        <p className="mt-4 text-gray-400">
+                            You have already played 3 games today.
+                            Come back tomorrow to play again.
+                        </p>
+
+                        <button
+                            onClick={() => setShowLimitModal(false)}
+                            className="
+                                mt-6
+                                rounded-lg
+                                border
+                                border-white
+                                px-10
+                                py-2.5
+                                font-semibold
+                                text-white
+                                transition
+                                hover:bg-[#3a3a3c]
+                                cursor-pointer
+                            "
+                        >
+                            OK
+                        </button>
+
+                    </div>
+
+                </div>
+            )}
+                
+            
 
         </div>
 
